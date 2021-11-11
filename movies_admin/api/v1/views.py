417e8279d4b3739c67951f6c935dfe6c4384e24c
Ekaterina Smorgun <e_smorgun@wargaming.net>
2021-11-12 01:48:06 +0300

    fin works

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -1,32 +1,33 @@
+import sys
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
-import sys
-sys.path.append('movies')
+sys.path.append("movies")
 from movies.models import FilmWork, PersonRoleType
 
 
 class MoviesApiMixin:
     model = FilmWork
-    http_method_names = ['get']
+    http_method_names = ["get"]
 
     def _aggregate_person(role: str):
         return ArrayAgg(
-            'filmworkperson__person_id__full_name',
+            "filmworkperson__person_id__full_name",
             filter=Q(filmworkperson__role=role),
             distinct=True,
         )
 
     @classmethod
     def get_queryset(cls):
-        return FilmWork.objects.values('id', 'title', 'description', 'creation_date', 'rating', 'type'
-                ).annotate(
-                    genres=ArrayAgg('genres__name', distinct=True),
-                    actors=cls._aggregate_person(role=PersonRoleType.ACTOR),
-                    directors=cls._aggregate_person(role=PersonRoleType.DIRECTOR),
-                    writers=cls._aggregate_person(role=PersonRoleType.WRITER)
+        return FilmWork.objects.values(
+            "id", "title", "description", "creation_date", "rating", "type"
+        ).annotate(
+            genres=ArrayAgg("genres__name", distinct=True),
+            actors=cls._aggregate_person(role=PersonRoleType.ACTOR),
+            directors=cls._aggregate_person(role=PersonRoleType.DIRECTOR),
+            writers=cls._aggregate_person(role=PersonRoleType.WRITER),
         )
 
     @staticmethod
@@ -36,49 +37,28 @@ class MoviesApiMixin:
 
 class Movies(MoviesApiMixin, BaseListView):
     paginate_by = 50
-    ordering = 'title'
+    ordering = "title"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         paginator, page, queryset, is_paginated = self.paginate_queryset(
-            self.get_queryset(),
-            self.paginate_by
+            self.get_queryset(), self.paginate_by
         )
         return {
-            'count': paginator.count,
-            'total_pages': paginator.num_pages,
-            'page': page.number,
-            'prev': page.previous_page_number() if page.has_previous() else None,
-            'next': page.next_page_number() if page.has_next() else None,
-            'results': list(queryset),
+            "count": paginator.count,
+            "total_pages": paginator.num_pages,
+            "page": page.number,
+            "prev": page.previous_page_number() if page.has_previous() else None,
+            "next": page.next_page_number() if page.has_next() else None,
+            "results": list(queryset),
         }
 
 
 class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
     def get_object(self, queryset=None):
         try:
-            return self.get_queryset().filter(id=self.kwargs['pk']).get()
+            return self.get_queryset().filter(id=self.kwargs["pk"]).get()
         except FilmWork.DoesNotExist as err:
             return None
 
     def get_context_data(self, object, **kwargs):
-        """
-        Details of a single movie
-        Raise 404 exception if ID is not found """
-        print(object)
-        try:
-            context = {
-                'results': object,
-            }
-        except Exception as err:
-            print(err)
-            print("Exception!!")
-
-        return context
-
-    #def get_context_data(self, **kwargs):
-        #return super().get_context_data(**kwargs)['object']
-
-        #one_data = self.get_queryset().filter(id=self.kwargs['pk'])
-        #return {
-        #    'results': self.get_queryset().filter(id=self.kwargs['pk']),
-        #}
+        return object
